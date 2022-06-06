0777dc9180326c4719eda7b50775b06483ab299c
m1dnas <felipesandimguliermone@gmail.com>
2022-06-06 09:58:08 -0700

    Task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
-/* using the function puts */
+/**
+ * main - print text
+ * Return: its OK
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
