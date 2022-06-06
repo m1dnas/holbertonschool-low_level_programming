763915dac1b99e839c58218781dbac6186dda20f
m1dnas <felipesandimguliermone@gmail.com>
2022-06-06 10:43:41 -0700

    Task 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -16,6 +16,6 @@ int main(void)
 	printf("Size of an int: %zu bytes(s)\n", sizeof(intType));
 	printf("Size of a long int: %zu bytes(s)\n", sizeof(lintType));
 	printf("Size of a long long int: %zu bytes(s)\n", sizeof(llintType));
-	printf("Size of a float: %zu bytes\n", sizeof(floatType));
+	printf("Size of a float: %zu bytes(s)\n", sizeof(floatType));
 	return (0);
 }
