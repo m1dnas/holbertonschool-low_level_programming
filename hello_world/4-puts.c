0aba47779d4a1b0304367b0a176700ed98a5c6e7
m1dnas <felipesandimguliermone@gmail.com>
2022-06-06 09:27:24 -0700

    Task 4

---
+++ hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Prints Programming is like building a multilingual puzzle #
+# include <stdio.h>
+int main ()
+{ 
+	int puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}	
