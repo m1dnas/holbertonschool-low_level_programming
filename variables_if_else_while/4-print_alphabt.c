7b3da61903f87558fe9413c6565ba4c07f1b0a83
m1dnas <felipesandimguliermone@gmail.com>
2022-06-07 08:31:27 -0700

    Task 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - test
+ * Return: its OK
+ **/
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	for (ch != 'q' ; 'e';)
+{
+	putchar(ch);
+}
+	putchar('\n');
+return (0);
+}
