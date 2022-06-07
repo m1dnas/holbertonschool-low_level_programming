4f3924b918b46741672d4d19362397f9f022d1de
m1dnas <felipesandimguliermone@gmail.com>
2022-06-07 08:56:47 -0700

    Task 4

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -8,8 +8,9 @@ int main(void)
 {
 	char ch;
 
-	for (ch = 'a' ; ch = !q && ch <= 'z' ; ch++)
-{
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	if (ch != 'e' && ch != 'q')
+	{
 	putchar(ch);
 }
 	putchar('\n');
