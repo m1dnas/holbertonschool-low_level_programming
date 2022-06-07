f2c935df72fe4d5e4836352fa58389964650c2b0
m1dnas <felipesandimguliermone@gmail.com>
2022-06-07 07:51:56 -0700

    Task 3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prueba123
+ * Return: its OK
+ **/
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'Z' ; ch++)
+{
+	putchar(ch);
+}
+	for (ch = 'A' ; ch <= 'Z' ; ch++)
+{
+	putchar(ch);
+}
+return (0);
+}
