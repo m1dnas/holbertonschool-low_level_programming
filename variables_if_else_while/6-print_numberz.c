f9af14685e93d41a8124242abadf4bbd8723dc90
m1dnas <felipesandimguliermone@gmail.com>
2022-06-07 09:26:21 -0700

    Task 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/** 
+ * main - entry point
+ * Return: returned succesfully
+ **/
+int main(void)
+{
+	int num;
+
+	for (num = '0' ; num <= '9' ; num++)
+	{
+	putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
