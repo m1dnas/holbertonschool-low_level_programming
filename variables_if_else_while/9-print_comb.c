2dfc23fa9c176f6f31cb4fb7e8e47bee646bb9d4
m1dnas <felipesandimguliermone@gmail.com>
2022-06-07 10:34:10 -0700

    Task 9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *  * main - test
+ *   * Return: its OK
+ *    **/
+int main(void)
+{
+	int x;
+
+	for (x = 48 ; x <= 57 ; x++)
+	{
+	putchar(n);
+	if (n < 57)
+	{
+	putchar(',');
+	putchar('\n');
+	}
+	}
+	putchar('\n');
+	return (0);
+}
