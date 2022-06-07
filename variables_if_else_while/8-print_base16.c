d643b87be51593787633166a81f254a1633e715c
m1dnas <felipesandimguliermone@gmail.com>
2022-06-07 09:44:12 -0700

    Task 8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *  *  * main - entry point
+ *   *   * Return: succesfully
+ *    *    **/
+int main(void)
+{
+	char n;
+
+	for (n = '0' ; n <= '9' ; n++)
+	{
+	putchar(n);
+	}
+	for (n = 'a' ; n <= 'f' ; n++)
+	{
+	putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
