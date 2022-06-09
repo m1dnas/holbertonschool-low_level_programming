94efdd6a53a60ce438b89067a88e3e9b20087ebb
m1dnas <felipesandimguliermone@gmail.com>
2022-06-09 14:54:48 -0700

    Task 8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * starting from 00:00 to 23:59.
  *
  **/
 void jack_bauer(void)
@@ -15,6 +16,7 @@ void jack_bauer(void)
 			_putchar(h / 10 + '0');
 			_putchar(h % 10 + '0');
 			_putchar(':');
+			_putchar('\n');
 		}
 	}
 }
