4b8686fcc535fa8758d91e8448f9c34e01057a73
m1dnas <felipesandimguliermone@gmail.com>
2022-06-08 20:45:31 -0700

    Task 0

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,1 +1,11 @@
-int _putchar(int character);
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ ** _putchar - writes the character c to std
+ ** @c: parameter to compare
+ ** Return: 1 or 0
+ **/
+int _putchar(char);
+
+#endif
