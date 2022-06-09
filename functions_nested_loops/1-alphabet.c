06c1b987da7a12f9d00a2388558c36455bd87ac4
m1dnas <felipesandimguliermone@gmail.com>
2022-06-08 21:01:28 -0700

    Task 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet
+ * Return: its 0
+ **/
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+	return (0);
+}
