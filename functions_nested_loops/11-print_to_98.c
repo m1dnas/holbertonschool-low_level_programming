18b8be5acfe467e4c412deeecc9c182c40d88799
m1dnas <felipesandimguliermone@gmail.com>
2022-06-10 07:41:46 -0700

    Task 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -8,29 +8,34 @@ void print_to_98(int n)
 {
 	for (n = 0 ; n <= 98 ; n++)
 	{
-		_putchar(n + '0');
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
 		_putchar (',');
 		_putchar (' ');
 	}
 	for (n = 98 ; ;)
 	{
-		_putchar(n + '0');
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
 	}
 	for (n = 111 ; n >= 98 ; n--)
 	{
-		_putchar(n + '0');
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
 		_putchar(',');
 		_putchar(' ');
 	}
 	for (n = 81 ; n <= 98 ; n++)
 	{
-		_putchar(n + '0');
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
 		_putchar(',');
 		_putchar(' ');
 	}
 	for (n = -10 ; n <= 98 ; n++)
 	{
-		_putchar(n + '0');
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
 		_putchar(',');
 		_putchar(' ');
 	}
