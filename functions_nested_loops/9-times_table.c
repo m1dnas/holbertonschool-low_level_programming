0088b4b50fe8cc2218c6886305addac0e7b5d30f
m1dnas <felipesandimguliermone@gmail.com>
2022-06-10 06:10:40 -0700

    Task 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -7,20 +7,19 @@
 void times_table(void)
 {
 	int x, y;
-	int n= y * x;
 
 	for (y = 0; y <= 9 ; y++)
 	{
 		for (x = 0; x <= 9 ; x++)
 		{
-				if (n <= 9)
+				if ((x * y) <= 9)
 				{
 					_putchar(n + '0');
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 				}
-				if (n > 9)
+				if ((x * y) > 9)
 				{
 					_putchar(n + '0');
 					_putchar(',');
