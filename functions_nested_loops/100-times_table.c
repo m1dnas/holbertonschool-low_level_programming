a2b78758d08077f488f3aa387c6b5454b6c69dc6
m1dnas <felipesandimguliermone@gmail.com>
2022-06-10 10:11:02 -0700

    m

---
+++ functions_nested_loops/100-times_table.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_times_table - prints the n times table
+ * @n: parameter to print the times table
+ *
+ **/
+void print_times_table(int n)
+{
+	int a = b * c;
+
+	while (n => 1 && n <= 15)
+	{
+		if (a == 0)
+		{
+			printf("0");
+		}
+	}
+}
