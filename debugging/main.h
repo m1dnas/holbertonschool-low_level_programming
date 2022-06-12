9d00d32b10280daa1737612e27a83d6d026ab795
m1dnas <felipesandimguliermone@gmail.com>
2022-06-12 11:11:49 -0700

    Task 0

---
+++ debugging/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+
+int largest_number(int a, b, c);
+
+void print_remaining_days(int month, day, year);
+
+int convert_day(int month, day);
+
+#endif
