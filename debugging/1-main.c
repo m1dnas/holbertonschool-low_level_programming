a812a011ed669072d5233bbed3b9bde194e14554
m1dnas <felipesandimguliermone@gmail.com>
2022-06-12 11:29:21 -0700

    Task 0

--- debugging/1-main.c
+++ debugging/1-main.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 /**
- * * main - causes an infinite loop
- * * Return: 0
- * */
+ * main - causes an infinite loop
+ * Return: 0
+ **/
 
 int main(void)
 {
 	int i;
 
-        printf("Infinite loop incoming :(\n");
+	printf("Infinite loop incoming :(\n");
 
-        i = 0;
+	i = 0;
 /*
 *       while (i < 10)
 *	{
@@ -20,5 +20,5 @@ int main(void)
 */
 	printf("Infinite loop avoided! \\o/\n");
 
-        return (0);
+	return (0);
 }
