ae73be931f4748c9452596a064fccaece4c4ec95
m1dnas <felipesandimguliermone@gmail.com>
2022-06-12 19:59:52 -0700

    task 10

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -14,7 +14,7 @@ void print_triangle(int size)
 		_putchar('\n');
 	for (a = 1; a <= size; a++)
 	{
-		for (b = 1; a <= size; b++)
+		for (b = 1; b <= size; b++)
 		{
 			if (b <= (size - a))
 				_putchar(' ');
