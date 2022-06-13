109ce44816a35b47625f43ecc5bfca00703bd52c
m1dnas <felipesandimguliermone@gmail.com>
2022-06-13 11:21:05 -0700

    task 12

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -27,13 +27,13 @@ void print_times_table(int n)
 				{
 					printf("  %d", c);
 				}
-				else if (c >= 100 && b != 0)
-				{
-					printf("%d", c);
-				}
 				else if (c >= 10 && c < 100)
 				{
 					printf(" %d", c);
+				}
+				else if (c >= 100 && b != 0)
+				{
+					printf("%d", c);
 				}
 			}
 			printf("\n");
