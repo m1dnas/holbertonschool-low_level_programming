e87028f89ed5cc75cd570c0d399e27f1976a4000
m1dnas <felipesandimguliermone@gmail.com>
2022-06-12 17:19:38 -0700

    task 5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -14,8 +14,8 @@ void more_numbers(void)
 		for (a = 0; a <= 14; a++)
 		{
 			if (a >= 10)
-				_putchar(a / 10 + '0');
-				_putchar(a % 10 + '0');
+			_putchar(a / 10 + '0');
+			_putchar(a % 10 + '0');
 		}
 		_putchar('\n');
 	}
