331645c39210e8ab46960c2dd2c40afea9e954a6
m1dnas <felipesandimguliermone@gmail.com>
2022-06-14 10:40:54 -0700

    task 4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -11,13 +11,14 @@ void print_rev(char *s)
 {
 	int i, length, w;
 	length = strlen(s);
+	int s[i];
 
 	for (i = 0; i < length/2; i++)
 	{
 		w = s[i];
 		s[i] = s[length - i - 1];
 		s[length - i - 1] = w;
-		printf("%d", w);
+		printf("%s", w);
 	}
 	printf("\n");
 }
