0892e9d4dcc0c7b548b558afcdca431a014fbde9
m1dnas <felipesandimguliermone@gmail.com>
2022-06-15 10:30:09 -0700

    task 8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -15,8 +15,12 @@ void print_array(int *a, int n)
 	for (i = 0; i < n; i++)
 	{
 		if (!(i = n - 1))
-		printf("%d, ", a[i]);
+		{
+			printf("%d, ", a[i]);
+		}
 		if (i = n - 1)
-		printf("%d", a[i]);
+		{
+			printf("%d", a[i]);
+		}
 	}
 }
