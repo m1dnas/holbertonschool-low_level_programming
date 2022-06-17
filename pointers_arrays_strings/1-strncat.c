3eac93b62c965ec923352c334e8c18d37849121a
m1dnas <felipesandimguliermone@gmail.com>
2022-06-17 12:24:34 -0700

    task 11

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -14,9 +14,9 @@ char *_strncat(char *dest, char *src, int n)
 	for (j = 0; dest[j] != '\0'; j++)
 	{
 	}
-	for (i = 0; i < n || src[i] != '\0'; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-		dest[j + n] = src[i];
+		dest[j + n] = src[n];
 	}
 	return (dest);
 }
