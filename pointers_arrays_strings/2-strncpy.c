c1e3471adb93a546c8ae513ea3e7b864c32fb8ef
m1dnas <felipesandimguliermone@gmail.com>
2022-06-17 13:24:03 -0700

    task 22

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -10,18 +10,15 @@
  **/
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i, j;
+	int i;
 
-	for (j = 0; dest[j] != '\0'; j++)
-        {
-        }
         for (i = 0; i < n && src[i] != '\0'; i++)
         {
-		if (src[i] < n)
-		{
-			putchar('-');
-		}
-                dest[j + i] = src[i];
+		dest[i] = src[i];
         }
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
         return (dest);
-}	
+}
