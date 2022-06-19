857a07b1aed29c486d1dceb5d5b1617ef35c0ee8
m1dnas <felipesandimguliermone@gmail.com>
2022-06-18 21:07:56 -0700

    task 44

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * reverse_arrray - reverses the content of an array of integers
+ * reverse_array - reverses the content of an array of integers
  * @a: an array of integers
  * @n: number of elements of the array
  * Return: void
