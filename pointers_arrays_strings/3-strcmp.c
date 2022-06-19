bcd151c347b7396514c3a7ed77217a2562ebd6ff
m1dnas <felipesandimguliermone@gmail.com>
2022-06-18 17:50:02 -0700

    task 33

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -13,9 +13,8 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	while (s1[i] - s2[i] == 0 && s1[i] != '\0')
+	for (i = 0; s1[i] - s2[i] == 0 && s1[i] != '\0'; i++)
 	{
-		i++;
 	}
 	return (s1[i] - s2[i]);
 }
