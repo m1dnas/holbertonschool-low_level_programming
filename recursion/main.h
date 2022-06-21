e62668abefaab33228eb933ff964d9de2e9d2717
m1dnas <felipesandimguliermone@gmail.com>
2022-06-20 23:32:43 -0700

    task 0

--- recursion/main.h
+++ recursion/main.h
@@ -1,20 +1,20 @@
 #ifndef MAIN_H
 #define MAIN_H
-// put a character
+/* put a character */
 char _putchar(char c);
-// prints a string
+/* prints a string */
 void _puts_recursion(char *s);
-// prints a string in reverse
+/* prints a string in reverse */
 void _print_rev_recursion(char *s);
-// returns the length of a string
+/* returns the length of a string */
 int _strlen_recursion(char *s);
-// returns the factorial of a given number
+/* returns the factorial of a given number */
 int factorial(int n);
-// returns the value of x raised to the power of y
+/* returns the value of x raised to the power of y */
 int _pow_recursion(int x, int y);
-// returns the natural square root of a number
+/* returns the natural square root of a number */
 int _sqrt_recursion(int n);
-//  returns 1 if the input integer is a prime number, otherwise return 0
+/*  returns 1 if the input integer is a prime number, otherwise return 0 */
 int is_prime_number(int n);
 
 #endif
