c7a109b6e466fe50cedbdb89e200b34dda652b91
m1dnas <felipesandimguliermone@gmail.com>
2022-06-21 16:30:05 -0700

    task 4

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include
+
+/**
+ * _pow_recursion - returns the value of x raised to the power of y
+ * @x: floating point base value
+ * @y: floating point power value
+ * Return: the value
+ **/
+int _pow_recursion(int x, int y)
+{
+	int i;
+
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y >= 0)
+	{
+	_pow_recursion(x, y);
+	y--;
+	return (x * x);
+	}
+}
