f66df51ae216d3ca33c1d7cd1ead1cd42d6c0e9e
m1dnas <felipesandimguliermone@gmail.com>
2022-06-21 18:49:58 -0700

    task 5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,5 +1,26 @@
 #include "main.h"
-#include <math.h>
+
+/**
+ * _sqrt - test it
+ * @n: square root
+ * @j: integral
+ * Return: the square root of numb
+ **/
+int _sqrt(int i, int n)
+{
+	if (i * i == n)
+	{
+		return (i);
+	}
+	else if (i > n / 2)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (_sqrt(i + 1, n));
+	}
+}
 
 /**
  * _sqrt_recursion - returns the natural square root of a number
@@ -8,18 +29,5 @@
  **/
 int _sqrt_recursion(int n)
 {
-	int i;
-
-	if (n != sqrt)
-	{
-		return (-1);
-	}
-	else if (n == sqrt)
-	{
-		return (1);
-	}
-	else
-	{
-	return (i = sqrt(n)):
-	}
+	return (_sqrt(1, n));
 }
