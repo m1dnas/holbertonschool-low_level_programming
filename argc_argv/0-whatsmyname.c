d01b6ff35b90e542a1626c03eb859c195ad681ae
m1dnas <felipesandimguliermone@gmail.com>
2022-06-21 19:17:51 -0700

    task 0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints its name
+ * @argc: argc
+ * @argv: argv
+ * Return: 0 if it's ok
+ */
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
