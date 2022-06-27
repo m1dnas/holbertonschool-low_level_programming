1be416ca3645b2789cc1fa36fc378b4554facb1e
m1dnas <felipesandimguliermone@gmail.com>
2022-06-26 17:58:50 -0700

    task 3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -2,11 +2,12 @@
 #include <stdlib.h>
 
 /**
- * alloc_grid - a pointer to a 2 dimensional array of integers
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers
  * @width: columns
  * @height: rows
  * Return: a pointer to a 2 dimensional array of integers
- **/
+ */
+
 int **alloc_grid(int width, int height)
 {
 	int **grid, row, col;
