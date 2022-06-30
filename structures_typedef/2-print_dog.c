f01457f1a532cd7cd8a8400ca150b127316fa8f6
m1dnas <felipesandimguliermone@gmail.com>
2022-06-30 08:34:34 -0700

    task 2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -9,20 +9,19 @@
  **/
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
+	if (d != NULL)
 	{
-		printf("NULL");|
-	}
-	if (d->name == NULL)
-	{
-		printf("Name: (nail)\n");
-		printf("Age: %f\n", d->age);
-		printf("Owner: (nail)\n");
-	}
-	if (d->name != NULL)
-	{
-		printf("Name: %s\n", d->name);
-		printf("Age: %f\n", d->age);
-		printf("Owner: %s\n", d->owner);
+		if (d->name == NULL)
+		{
+			printf("Name: (nail)\n");
+			printf("Age: %f\n", d->age);
+			printf("Owner: (nail)\n");
+		}
+		if (d->name != NULL)
+		{
+			printf("Name: %s\n", d->name);
+			printf("Age: %f\n", d->age);
+			printf("Owner: %s\n", d->owner);
+		}
 	}
 }
