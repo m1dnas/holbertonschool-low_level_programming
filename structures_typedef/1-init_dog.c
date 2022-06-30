f0d2e5057b6f9d39fbe7d3ffdc5d6bc641e600d7
m1dnas <felipesandimguliermone@gmail.com>
2022-06-30 06:35:03 -0700

    task 1

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+
+/**
+ * init_dog - initialize a variable of type struct dog
+ * @dog: dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Return: void
+ **/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog *d;
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
