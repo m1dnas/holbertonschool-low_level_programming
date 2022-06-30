195292a738e5d7e47760e0179dd6d51f8ea277c6
m1dnas <felipesandimguliermone@gmail.com>
2022-06-30 06:50:20 -0700

    task 2

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * print_dog - prints a struct dog
+ * @d: d
+ * Return: void
+ **/
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+	}
+	if (name == NULL)
+	{
+		printf("Name: (nil)");
+	}
+}
