c8a7b163c121fc5808bf9fb7f5a0ffa25e124a7d
m1dnas <felipesandimguliermone@gmail.com>
2022-06-30 08:53:44 -0700

    task 3

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -16,6 +16,6 @@ struct dog
  * struct dog dog_t - structure of dog_t
  **/
 typedef struct dog dog_t;
-void init_dog(struct dog_t *d, char *name, float age, char *owner);
-void print_dog(struct dog_t *d);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 #endif
