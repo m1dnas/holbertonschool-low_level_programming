8a03e829cac88a4066efc404f24f7aa12d39e63b
m1dnas <felipesandimguliermone@gmail.com>
2022-06-30 08:56:15 -0700

    task 3

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -13,7 +13,7 @@ struct dog
 	char *owner;
 };
 /**
- * struct dog dog_t - structure of dog_t
+ * dog dog_t - structure of dog_t
  **/
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
