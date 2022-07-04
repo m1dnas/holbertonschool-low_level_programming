b411fd29f918f1831fdbc6c8baea1e8e6c0ef47e
m1dnas <felipesandimguliermone@gmail.com>
2022-07-03 19:43:04 -0700

    task

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -2,8 +2,9 @@
 #include
 
 /**
- *
- *
+ * sum_them_all - returns the sum of all its parameters
+ * @n: parameter 1
+ * Return: if n = 0, returns 0
  **/
-int sum_them_all(const unsigned int n, ...);
+int sum_them_all(const unsigned int n, ...)
 {
