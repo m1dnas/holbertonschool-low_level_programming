4d0ff8956ac5bedd881398dc53cd244e0e0f5ef0
m1dnas <felipesandimguliermone@gmail.com>
2022-07-13 11:47:59 -0700

    task 1

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a linked list
+ * @h: nude
+ * Return: the number of elements in a linked list
+ */
+size_t list_len(const list_t *h)
+{
+	while (h->next != NULL)
+	{
+		return(list_len(h->next)+ 1);
+	}
+	return(1);
+}
