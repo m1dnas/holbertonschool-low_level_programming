12ce6803bcb3faad6d453b37cc1f305a7d22ffe3
m1dnas <felipesandimguliermone@gmail.com>
2022-07-13 11:14:52 -0700

    task 0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -9,9 +9,18 @@ size_t print_list(const list_t *h)
 {
 	size_t size = 0;
 	
-	while (h->next != NULL)
+	if (h)
 	{
 		size++;
+		if (h->next != NULL)
+		{
+			printf("[%d] (%s)\n", h->len, h->str);
+		}
+		if (h->str)
+		{
+			printf("[0] (nil)\n");
+		}
+
 	}
 	return (size);
 }
--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -2,6 +2,14 @@
 #define LISTS_H
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
 typedef struct list_s
 {
 	char *str;
