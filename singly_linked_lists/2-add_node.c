6c8a055acc4501064a4afd04115a8c5b00bd076a
m1dnas <felipesandimguliermone@gmail.com>
2022-07-14 15:27:47 -0700

    task 2

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a new node at the beggining of a list
+ * @head: the head of the list
+ * @str: the string
+ * Return: the address of the new element, or NULL if it failed
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+
