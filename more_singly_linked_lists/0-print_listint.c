81b1a2fe0509dc53e3d9ce80a2d4c837cbf62002
m1dnas <felipesandimguliermone@gmail.com>
2022-07-15 15:23:21 -0700

    task 0

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -1,18 +1,18 @@
 #include "lists.h"
 /**
- * print_listint - function that prints all the elements of a list_t list
- * @h: the list
- * Return: the number of nodes
+ * listint_len - length of linked list
+ * @h: struct to go through
+ * Return: amount of elements
  */
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 	size_t count = 0;
 
 	while (h != NULL)
 	{
 		count++;
-		printf("%d\n", h->n);
-		count = h->next;
+		h = h->next;
 	}
+
 	return (count);
 }
