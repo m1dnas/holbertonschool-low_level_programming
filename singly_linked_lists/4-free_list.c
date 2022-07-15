15b6c1b6a022e97055dfc1f36482d31891316b69
m1dnas <felipesandimguliermone@gmail.com>
2022-07-15 14:27:51 -0700

    task 4

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_list - frees a list
+ * @head: head var
+ */
+void free_list(list_t *head)
+{
+	list_t *temporary;
+
+	while (head != NULL)
+	{
+		temporary = head->next;
+		free(head->str);
+		free(head);
+		head = temporary;
+	}
+}
