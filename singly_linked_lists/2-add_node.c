442e10269a9886f7906d7781076713ccbc1e40ce
m1dnas <felipesandimguliermone@gmail.com>
2022-07-15 14:27:22 -0700

    task

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -8,4 +8,22 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
+	list_t *new;
+	int i = 0;
 
+	while (str[i])
+		i++;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->str = strdup(str);
+	new->len = i;
+	new->next = *head;
+
+	*head = new;
+
+	return (new);
+}
