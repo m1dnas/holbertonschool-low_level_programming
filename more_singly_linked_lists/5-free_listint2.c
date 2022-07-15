15cdf7870280470de0adbdf42aa3619b52158e8b
m1dnas <felipesandimguliermone@gmail.com>
2022-07-15 15:32:19 -0700

    task 5

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -1,20 +1,21 @@
 #include "lists.h"
 /**
- * free_listint2 - frees a list but head is null
- * @head: linked list and head
- *
+ * free_listint2 -  free all the elements of the list
+ * @head: the first element of the list
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *tmp;
+	listint_t *aux;
 
-	if (head == NULL)
-		return;
-	while (*head != NULL)
+	if (head)
 	{
-		tmp = *head;
-		free(*head);
-		*head = tmp->next;
+	while (*head)
+	{
+	aux = *head;
+	*head = (*head)->next;
+	free(aux);
 	}
+	free(*head);
 	*head = NULL;
+	}
 }
