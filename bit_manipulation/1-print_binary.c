5402864bbf47a972cf63a96553d342564339d74b
m1dnas <felipesandimguliermone@gmail.com>
2022-07-20 18:06:20 -0700

    task 1

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_binary - Write a function that prints the binary representation
+ * of a number.
+ * @n: binary
+ * Return: (converted to binary)
+ */
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
+}
