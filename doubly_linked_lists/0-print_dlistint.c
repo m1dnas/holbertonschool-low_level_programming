8196643a82223f3f81bbca4bbcf11158cc63b6ab
m1dnas <felipesandimguliermone@gmail.com>
2022-07-22 13:53:36 -0700

    doubly linked lists

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -2,7 +2,18 @@
 
 /**
  * print_dlistint - prints all the elements of a list
- * @h: 
- *
+ * @h: header
+ * Return: the number of nodes
  **/
-size_t print_dlistint(const dlistint_t *h);
+size_t print_dlistint(const dlistint_t *h)
+{
+	int i = 0;
+	
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
