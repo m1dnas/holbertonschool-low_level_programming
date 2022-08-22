7f5d924d9e999153850e7c3a288111908b1e9e14
m1dnas <felipesandimguliermone@gmail.com>
2022-08-21 19:29:32 -0700

    betty fixes

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,13 +1,13 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - creates a hash table 
- * 
+ * hash_table_create - creates a hash table
+ *
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *newtable;
-    
+
 	if (size == 0)
 		return (NULL);
 
